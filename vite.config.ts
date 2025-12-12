af99cdf0fc1af51e0b821a8a92332635b59bfd08
osamanather3211-sys <osamanather3211@gmail.com>
2025-12-12 17:24:11 +0300

    Updaate vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,13 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import path from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  loadEnv(mode, '.', '');
-  return {
-    base: '/',
-    server: {
-      port: 3000,
-      host: '0.0.0.0',
-    },
-    plugins: [react()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      }
+export default defineConfig({
+  base: '/',
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
     }
-  };
-});
+  }
+})
